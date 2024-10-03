cab8406ba9b947e13ec10b0820c33ba0a17fae5b
ArtemRodnaev <ihtiandr123.com@yandex.ru>
2024-10-04 00:56:08 +0500

    проекс с проектами 1.0.1

--- src/finalProject.jsx
+++ src/finalProject.jsx
@@ -2,19 +2,22 @@ import React, { useState } from "react";
  import UsersList from "./Components/UserListComponent/UsersListApp";
  import { Slider } from "./Components/SliderComponent/SliderApp";
  import Legend from "./Components/legendComponent/legendApp";
-// window<1? setWindow(1) : window>3? setWindow(3) : window===1? <UsersList/> : window===2? <Legend/> : <Slider/>
 // import { Auth } from "./Components/RegFormCoponent/Auth";
 import { RegApp } from "./Components/RegFormCoponent/RegApp";
 
 
 export const App = () => {
-    const [window, setWindow] = useState(3)
+    const [demo, setDemo] = useState(4)
 return(
     <> 
-    {(window<1? setWindow(1) : window>3? setWindow(3) : window===1? <UsersList/> : window===2? <Legend/> : <Slider/>)}  
-    <div style={{width:"40px", backgroundColor:"orange", border:"1px solid black"}} onClick={()=>setWindow(1)}>1 </div> 
-    <div style={{width:"40px", backgroundColor:"orange", border:"1px solid black"}} onClick={()=>setWindow(2)}>2</div> 
-    <div  style={{width:"40px", backgroundColor:"orange", border:"1px solid black"}}  onClick={()=>setWindow(3)}>3</div> 
+    {(demo == 1 ?  <UsersList/> 
+    : demo == 2 ? <Legend/> 
+    : demo == 3 ? <Slider/>
+    : demo == 4 && <RegApp/>)}  
+    <div style={{width:"40px", backgroundColor:"orange", color: "black",  border:"1px solid black"}} onClick={()=>setDemo(1)}>1 проект</div> 
+    <div style={{width:"40px", backgroundColor:"orange", color: "black",  border:"1px solid black"}} onClick={()=>setDemo(2)}>2 проект</div> 
+    <div style={{width:"40px", backgroundColor:"orange",  color: "black", border:"1px solid black"}}  onClick={()=>setDemo(3)}>3 проект</div> 
+    <div style={{width:"40px", backgroundColor:"orange",  color: "black", border:"1px solid black"}}  onClick={()=>setDemo(4)}>4 проект</div> 
     </>
 )
 }
